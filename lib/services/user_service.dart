2aba9ad25aa4b0a1f53625756205e010468986d2
HumaCode <HumaCode>
2023-09-08 19:23:24 +0700

    membuat fungsi login

---
+++ lib/services/user_service.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+import 'package:flutter_blog/constants.dart';
+import 'package:flutter_blog/models/api_response.dart';
+import 'package:flutter_blog/models/user.dart';
+import 'package:http/http.dart' as http;
+
+// fungsi login
+Future<ApiResponse> login(String email, String password) async {
+  ApiResponse apiResponse = ApiResponse();
+
+  try {
+    final response = await http.post(Uri.parse(loginUrl), headers: {
+      'Accept': 'application/json',
+    }, body: {
+      'email': email,
+      'password': password,
+    });
+
+    switch (response.statusCode) {
+      case 200:
+        apiResponse.data = User.fromJson(jsonDecode(response.body));
+        break;
+      case 422:
+        final errors = jsonDecode(response.body)['errors'];
+        apiResponse.error = errors[errors.keys.elementAt(0)][0];
+        break;
+      case 403:
+        apiResponse.error = jsonDecode(response.body)['message'];
+        break;
+      default:
+        apiResponse.error = somethingWentWrong;
+    }
+  } catch (e) {
+    apiResponse.error = serverError;
+  }
+
+  return apiResponse;
+}
