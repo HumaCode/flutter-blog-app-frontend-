f360e2bba7dbb6d6d2115350eff091144b0a4539
HumaCode <HumaCode>
2023-09-08 19:06:50 +0700

    membuat constants

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+const baseUrl = 'http://192.168.5.10:8080/api';
+const loginUrl = '$baseUrl/login';
+const registerUrl = '$baseUrl/register';
+const logoutUrl = '$baseUrl/logout';
+const userUrl = '$baseUrl/user';
+const postsUrl = '$baseUrl/posts';
+const commentsUrl = '$baseUrl/comments';
+
+// error
+const serverError = 'Server Error';
+const unauthorized = 'Unauthorized';
+const somethingWentWrong = 'Something went wrong, try again';
