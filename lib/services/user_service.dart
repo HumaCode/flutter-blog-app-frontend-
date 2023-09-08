9df866a9548e8074888d89d5c511e2199ac591d0
HumaCode <HumaCode>
2023-09-08 19:36:49 +0700

    membuat fungsi register

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -37,3 +37,38 @@ Future<ApiResponse> login(String email, String password) async {
 
   return apiResponse;
 }
+
+// fungsi register
+Future<ApiResponse> register(String name, String email, String password) async {
+  ApiResponse apiResponse = ApiResponse();
+
+  try {
+    final response = await http.post(Uri.parse(registerUrl), headers: {
+      'Accept': 'application/json',
+    }, body: {
+      'name': name,
+      'email': email,
+      'password': password,
+      'password_confirmation': password,
+    });
+
+    switch (response.statusCode) {
+      case 200:
+        apiResponse.data = User.fromJson(jsonDecode(response.body));
+        break;
+      case 422:
+        final errors = jsonDecode(response.body)['errors'];
+        apiResponse.error = errors[errors.keys.elementAt(0)][0];
+        break;
+      case 403:
+        apiResponse.error = jsonDecode(response.body)['message'];
+        break;
+      default:
+        apiResponse.error = somethingWentWrong;
+    }
+  } catch (e) {
+    apiResponse.error = serverError;
+  }
+
+  return apiResponse;
+}
