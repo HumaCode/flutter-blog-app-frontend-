cc4a999289c2fd855426809049bf573f43cb15d6
HumaCode <HumaCode>
2023-09-08 20:25:55 +0700

    implementasi fungsi cek user - LoadingPage

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,4 +1,11 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_blog/constants.dart';
+import 'package:flutter_blog/models/api_response.dart';
+import 'package:flutter_blog/screens/auth/login.dart';
+import 'package:flutter_blog/screens/home.dart';
+import 'package:flutter_blog/services/user_service.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -8,6 +15,43 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  late BuildContext context;
+
+  // cek user
+  void _loadUserInfo() async {
+    String token = await getToken();
+
+    if (token == '') {
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(builder: (context) => const Login()),
+          (route) => false);
+    } else {
+      ApiResponse response = ApiResponse();
+
+      if (response.error == null) {
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => const HomePage()),
+            (route) => false);
+      } else if (response.error == unauthorized) {
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => const Login()),
+            (route) => false);
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text('${response.error}'),
+          ),
+        );
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    _loadUserInfo();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
