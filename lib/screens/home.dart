8636fe61a25298412fb031930365845e75dc01da
HumaCode <HumaCode>
2023-09-09 11:03:15 +0700

    membuat halaman home & implementasi fungsi logout

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog/screens/auth/login.dart';
+import 'package:flutter_blog/services/user_service.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +12,19 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: GestureDetector(
+        onTap: () {
+          logout().then((value) => {
+                Navigator.of(context).pushAndRemoveUntil(
+                    MaterialPageRoute(builder: (context) => const Login()),
+                    (route) => false)
+              });
+        },
+        child: const Center(
+          child: Text('HOME'),
+        ),
+      ),
+    );
   }
 }
