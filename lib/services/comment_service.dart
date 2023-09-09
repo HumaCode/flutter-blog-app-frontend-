960524072d74f6e6aa3cd1db494c5c9f5ae59343
HumaCode <HumaCode>
2023-09-09 23:38:31 +0700

    membuat fungsi create comment

--- lib/services/comment_service.dart
+++ lib/services/comment_service.dart
@@ -28,7 +28,41 @@ Future<ApiResponse> getComments(int postId) async {
         apiResponse.data as List<dynamic>;
         break;
       case 403:
+        apiResponse.error = jsonDecode(response.body)['message'];
+        break;
+      case 401:
+        apiResponse.error = unauthorized;
+        break;
+      default:
+        apiResponse.error = somethingWentWrong;
+    }
+  } catch (e) {
+    apiResponse.error = serverError;
+  }
+
+  return apiResponse;
+}
+
+// fungsi create comment
+Future<ApiResponse> createComments(int postId, String comment) async {
+  ApiResponse apiResponse = ApiResponse();
+
+  try {
+    String token = await getToken();
+    final response =
+        await http.post(Uri.parse('postsUrl/$postId/comments'), headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Bearer $token',
+    }, body: {
+      'comment': comment,
+    });
+
+    switch (response.statusCode) {
+      case 200:
         apiResponse.data = jsonDecode(response.body)['message'];
+        break;
+      case 403:
+        apiResponse.error = jsonDecode(response.body)['message'];
         break;
       case 401:
         apiResponse.error = unauthorized;
